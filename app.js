968dfea26abf5e1cb1894c5cc53601080c5fc59a
Petr Hurtak <petr.hurtak@gmail.com>
2022-11-03 10:05:34 +0100

    update

--- app.js
+++ app.js
@@ -3,44 +3,64 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+//
+// Config
+//
+
 const PORT = 3000;
 const COOKIE_NAME = "auth_test";
 
-const app = express();
+//
+// Express setup
+//
 
-// view engine setup
+const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//
+// Routes
+//
+
 app.get("/", function (req, res, next) {
+  res.setHeader("Cache-Control", "public, max-age=0");
+  res.setHeader("Expires", "Fri, 04 Nov 2022 07:59:12 GMT");
+
   res.render("index", {
     cookies: req.cookies,
     loggedIn: Boolean(req.cookies[COOKIE_NAME]),
+    time: new Date().toLocaleString(),
   });
 });
 
 app.post("/login", function (req, res, next) {
-  res.cookie(COOKIE_NAME, "yes", { maxAge: 900000, httpOnly: true });
+  res.cookie(COOKIE_NAME, "yes", {
+    maxAge: 900_000,
+    httpOnly: true,
+    secure: true,
+  });
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+
   res.redirect("/");
 });
 
-app.get("/logout", function (req, res, next) {
+const logout = function (req, res, next) {
   res.clearCookie(COOKIE_NAME);
-  res.redirect("/");
-});
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
 
-app.post("/logout", function (req, res, next) {
-  res.clearCookie(COOKIE_NAME);
   res.redirect("/");
-});
+};
+app.get("/logout", logout);
+app.post("/logout", logout);
 
-// error handler
+//
+// Error handler
+//
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -51,6 +71,10 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+//
+// Start the app
+//
+
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
